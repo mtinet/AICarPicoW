1fb1ad1ed125e5b5cb34642c7601eb8cee941ab4
Jason Juhyun Kim <mtinet@hanmail.net>
2025-08-24 18:10:39 +0900

    Update 3. stepMotorPicoW.py

--- 3. stepMotorPicoW.py
+++ 3. stepMotorPicoW.py
@@ -22,7 +22,7 @@ step_rst.value(0)      # RST: LOW로 초기화
 utime.sleep_ms(10)
 
 # 회전 함수
-def step_rotate(direction='left', degrees=20):
+def step_rotate(direction='left', degrees=25):
     global rotate_pos
 
     # 1 step = 0.036 degrees (with 1:50 gear), 여기에 실제 회전 각도 보정을 위해 ×2
@@ -54,12 +54,20 @@ def step_rotate(direction='left', degrees=20):
 
 
 
-# 테스트 루프: 왼쪽 1회, 오른쪽 1회 반복
+# 테스트 루프: 중앙 -> 왼쪽 13도 -> 중앙 -> 오른쪽 13도 -> 중앙 반복
 while True:
-    print("LEFT")
-    step_rotate('left')
+    print("--- Moving: CENTER to LEFT (13 degrees) ---")
+    step_rotate(direction='left', degrees=13)
     utime.sleep(1)
 
-    print("RIGHT")
-    step_rotate('right')
+    print("--- Moving: LEFT to CENTER (13 degrees) ---")
+    step_rotate(direction='right', degrees=13)
     utime.sleep(1)
+
+    print("--- Moving: CENTER to RIGHT (13 degrees) ---")
+    step_rotate(direction='right', degrees=13)
+    utime.sleep(1)
+
+    print("--- Moving: RIGHT to CENTER (13 degrees) ---")
+    step_rotate(direction='left', degrees=13)
+    utime.sleep(2) # 전체 사이클 반복 전 2초 대기
